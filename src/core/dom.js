6dabcbff46a303cdb52dfd45f944bebbbb32ab1b
Ilyas Gibadullin <ilyas.gibadullin.199819987@gmail.com>
2022-12-22 21:49:32 +0300

    meow

--- src/core/dom.js
+++ src/core/dom.js
@@ -97,10 +97,14 @@ class Dom {
 
   addClass(className) {
     this.$el.classList.add(className);
+
+    return this;
   }
 
   removeClass(className) {
     this.$el.classList.remove(className);
+
+    return this;
   }
 }
 
